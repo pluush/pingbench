1b1aed4d1f8f3daa07028f640507f5f42ac28e30
pluush <meow.meow55@yahoo.co.id>
2017-10-09 17:07:46 +0800

    Create pingbench.sh
    Initial version v0.3

---
+++ pingbench.sh
@@ -0,0 +1,294 @@
+########################################
+# Ping benchmark script by pluush      #
+# v0.3                                 #
+# Copyright(C) 2017 - pluush           #
+########################################
+
+pingcalc (){
+count=0 # Successful ping counts
+pin0=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin1=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin2=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin3=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin4=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin5=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin6=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin7=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin8=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin9=$(ping -w 2 $serv -c 1 | grep time= | awk -F = '{print $4}')
+pin0n=$(echo $pin0 | awk '{print $1}')
+pin1n=$(echo $pin1 | awk '{print $1}')
+pin2n=$(echo $pin2 | awk '{print $1}')
+pin3n=$(echo $pin3 | awk '{print $1}')
+pin4n=$(echo $pin4 | awk '{print $1}')
+pin5n=$(echo $pin5 | awk '{print $1}')
+pin6n=$(echo $pin6 | awk '{print $1}')
+pin7n=$(echo $pin7 | awk '{print $1}')
+pin8n=$(echo $pin8 | awk '{print $1}')
+pin9n=$(echo $pin9 | awk '{print $1}')
+if [ -z "$pin0n" ]; then count=$count; pin0n=0; else count=$[$count+1];fi
+if [ -z "$pin1n" ]; then count=$count; pin1n=0; else count=$[$count+1];fi
+if [ -z "$pin2n" ]; then count=$count; pin2n=0; else count=$[$count+1];fi
+if [ -z "$pin3n" ]; then count=$count; pin3n=0; else count=$[$count+1];fi
+if [ -z "$pin4n" ]; then count=$count; pin4n=0; else count=$[$count+1];fi
+if [ -z "$pin5n" ]; then count=$count; pin5n=0; else count=$[$count+1];fi
+if [ -z "$pin6n" ]; then count=$count; pin6n=0; else count=$[$count+1];fi
+if [ -z "$pin7n" ]; then count=$count; pin7n=0; else count=$[$count+1];fi
+if [ -z "$pin8n" ]; then count=$count; pin8n=0; else count=$[$count+1];fi
+if [ -z "$pin9n" ]; then count=$count; pin9n=0; else count=$[$count+1];fi
+if [ "$count" = 0 ]; then pin1n=0; pin2n=0; pin3n=0; pin4n=0; pin5n=0; pin6n=0; pin7n=0; pin8n=0; pin9n=0; pin0n=0; count=1; fi 
+res=$(echo "$pin0n $pin1n $pin2n $pin3n $pin4n $pin5n $pin6n $pin7n $pin8n $pin9n $count" | awk '{print $1/$11 + $2/$11 + $3/$11 + $4/$11 + $5/$11 + $6/$11 + $7/$11 + $8/$11 + $9/$11 + $10/$11}');
+if [ "$res" = "0" ]; then resd="Timeout"; else resd="$res ms"; scount=$[$scount+1]; scountl=$[$scountl+1]; fi 
+}
+
+ip=$( wget -qO- ipv4.icanhazip.com ) # Getting IPv4
+scount=0 # Non-timeout server count
+
+# Ping test via ping
+echo " "
+echo "-----------------------------------------------------------------"
+echo "Ping benchmark by Plush"
+echo "Your public IPv4 is $ip"
+echo "-----------------------------------------------------------------"
+echo " "
+echo " "
+echo "-----------------------------------------------------------------"
+echo "Test Region : CDN"
+scountl=0 # Reset region server count
+echo "Description			Provider		Avg ping"
+echo "-----------------------------------------------------------------"
+serv="8.8.8.8"; pingcalc; resc00=$res; 
+echo "Google DNS			Google			$resd"
+serv="cachefly.cachefly.net"; pingcalc; resc01=$res; 
+echo "Cachefly CDN			Cachefly		$resd"
+serv="cdn.teliacompany.com"; pingcalc; resc05=$res; 
+echo "Telia CDN Web			Telia			$resd"
+serv="cloudflare.com"; pingcalc; resc02=$res; 
+echo "Cloudflare Web			Cloudflare		$resd"
+serv="cloudflare.com"; pingcalc; resc03=$res; 
+echo "Akamai Web			Akamai			$resd"
+serv="facebook.com"; pingcalc; resc04=$res; 
+echo "Facebook Web			Facebook		$resd"
+echo "-----------------------------------------------------------------"
+cdnavg=$(echo "$resc00 $resc01 $resc02 $resc03 $resc04 $resc05 $scountl" | awk '{print $1/$7 + $2/$7 + $3/$7 + $4/$7 + $5/$7 + $6/$7}');
+echo "Successful servers : $scountl / Average ping : $cdnavg ms"
+cdnavgnorm=$(echo "$cdnavg" | awk '{print $1+20}');
+scorecdn=$(echo "$cdnavgnorm 200" | awk '{print exp(-$1/$2)*110.517091808}');
+echo "Score : $scorecdn"
+
+echo " "
+echo " "
+echo "-----------------------------------------------------------------"
+echo "Test Region : Americas"
+scountl=0 # Reset region server count
+echo "Location			Provider		Avg ping"
+echo "-----------------------------------------------------------------"
+serv="repos.lax-noc.com"; pingcalc; res000=$res; 
+echo "Los Angeles, CA, U.S.		QuadraNet		$resd"
+serv="lg.lax.psychz.net"; pingcalc; res001=$res; 
+echo "Los Angeles, CA, U.S.		Psychz			$resd"
+serv="lax-ca-us-ping.vultr.com"; pingcalc; res002=$res; 
+echo "Los Angeles, CA, U.S.		Choopa			$resd"
+serv="lg.la.ramnode.com"; pingcalc; res003=$res; 
+echo "Los Angeles, CA, U.S.		RamNode			$resd"
+serv="129.250.2.176"; pingcalc; res004=$res; 
+echo "Los Angeles, CA, U.S.		NTT (Router)		$resd"
+serv="speedtest.sjc01.softlayer.com"; pingcalc; res005=$res; 
+echo "San Jose, CA, U.S.		RamNode			$resd"
+serv="speedtest.fremont.linode.com"; pingcalc; res006=$res; 
+echo "Freemont, CA, U.S.		Linode			$resd"
+serv="speedtest-sfo1.digitalocean.com"; pingcalc; res007=$res; 
+echo "San Francisco, CA, U.S.		DigitalOcean		$resd"
+serv="sjo-ca-us-ping.vultr.com"; pingcalc; res008=$res; 
+echo "Silicon Valley, CA, U.S.	Choopa			$resd"
+serv="tx-us-ping.vultr.com"; pingcalc; res009=$res; 
+echo "Dallas, TX, U.S.		Choopa			$resd"
+serv="speedtest.dallas.linode.com"; pingcalc; res010=$res; 
+echo "Dallas, TX, U.S.		Linode			$resd"
+serv="lg.texas.psychz.net"; pingcalc; res011=$res; 
+echo "Dallas, TX, U.S.		Psychz			$resd"
+serv="il-us-ping.vultr.com"; pingcalc; res012=$res; 
+echo "Chicago, IL, U.S.		Choopa			$resd"
+serv="ga-us-ping.vultr.com"; pingcalc; res013=$res; 
+echo "Atlanta, GA, U.S.		Choopa			$resd"
+serv="speedtest.atlanta.linode.com"; pingcalc; res014=$res; 
+echo "Atlanta, GA, U.S.		Linode			$resd"
+serv="lg.atl.ramnode.com"; pingcalc; res015=$res; 
+echo "Atlanta, GA, U.S.		RamNode			$resd"
+serv="speed.atl.coloat.com"; pingcalc; res016=$res; 
+echo "Atlanta, GA, U.S.		Coloat			$resd"
+serv="wa-us-ping.vultr.com"; pingcalc; res017=$res; 
+echo "Seattle, WA, U.S.		Choopa			$resd"
+serv="lg.sea.ramnode.com"; pingcalc; res018=$res; 
+echo "Seattle, WA, U.S.		RamNode			$resd"
+serv="speedtest.sea01.softlayer.com"; pingcalc; res019=$res; 
+echo "Seattle, WA, U.S.		Softlayer		$resd"
+serv="nj-us-ping.vultr.com"; pingcalc; res020=$res; 
+echo "New York, NY, U.S.		Choopa			$resd"
+serv="lg.nyc.ramnode.com"; pingcalc; res021=$res; 
+echo "New York, NY, U.S.		RamNode			$resd"
+serv="speedtest-nyc3.digitalocean.com"; pingcalc; res022=$res; 
+echo "New York, NY, U.S.		DigitalOcean		$resd"
+serv="lg.buf.colocrossing.com"; pingcalc; res023=$res; 
+echo "Buffalo, NY, U.S.		ColoCrossing		$resd"
+serv="speedtest.newark.linode.com"; pingcalc; res024=$res; 
+echo "Newark, NJ, U.S.		Linode			$resd"
+serv="fl-us-ping.vultr.com"; pingcalc; res025=$res; 
+echo "Miami, FL, U.S.			Choopa			$resd"
+serv="speedtest.wdc01.softlayer.com"; pingcalc; res026=$res; 
+echo "Washington, DC, U.S.		Softlayer		$resd"
+serv="lg.va.psychz.net"; pingcalc; res027=$res; 
+echo "Ashburn, VA, U.S.		Psychz			$resd"
+serv="speedtest-tor1.digitalocean.com"; pingcalc; res028=$res; 
+echo "Toronto, ON, Canada		DigitalOcean		$resd"
+serv="198.50.175.241"; pingcalc; res029=$res; 
+echo "Montréal, QC, Canada		OVH			$resd"
+echo "-----------------------------------------------------------------"
+ameavg=$(echo "$res000 $res001 $res002 $res003 $res004 $res005 $res006 $res007 $res008 $res009 $res010 $res011 $res012 $res013 $res014 $res015 $res016 $res017 $res018 $res019 $res020 $res021 $res022 $res023 $res024 $res025 $res026 $res027 $res028 $res029 $scountl" | awk '{print $1/$31 + $2/$31 + $3/$31 + $4/$31 + $5/$31 + $6/$31 + $7/$31 + $8/$31 + $9/$31 + $10/$31 + $11/$31 + $12/$31 + $13/$31 + $14/$31 + $15/$31 + $16/$31 + $17/$31 + $18/$31 + $19/$31 + $20/$31 + $21/$31 + $22/$31 + $23/$31 + $24/$31 + $25/$31 + $26/$31 + $27/$31 + $28/$31 + $29/$31 + $30/$31}');
+echo "Successful servers : $scountl / Average ping : $ameavg ms"
+scoreame=$(echo "$ameavg 400" | awk '{print exp(-$1/$2)*100}');
+echo "Score : $scoreame"
+
+echo " "
+echo " "
+echo "-----------------------------------------------------------------"
+echo "Test Region : Europe"
+scountl=0 # Reset region server count
+echo "Location			Provider		Avg ping"
+echo "-----------------------------------------------------------------"
+serv="fra-de-ping.vultr.com"; pingcalc; res100=$res; 
+echo "Frankfurt, Germany		Choopa			$resd"
+serv="speedtest.frankfurt.linode.com"; pingcalc; res101=$res; 
+echo "Frankfurt, Germany		Linode			$resd"
+serv="130.117.254.89"; pingcalc; res110=$res; 
+echo "Berlin, Germany			Cogent (Router)		$resd"
+serv="ams-nl-ping.vultr.com"; pingcalc; res102=$res; 
+echo "Amsterdam, Netherlands		Choopa			$resd"
+serv="mirror.ams1.nl.leaseweb.net"; pingcalc; res103=$res; 
+echo "Amsterdam, Netherlands		Leaseweb		$resd"
+serv="lg.ams.psychz.net"; pingcalc; res104=$res; 
+echo "Amsterdam, Netherlands		Psychz			$resd"
+serv="par-fr-ping.vultr.com"; pingcalc; res105=$res; 
+echo "Paris, France			Choopa			$resd"
+serv="lon-gb-ping.vultr.com"; pingcalc; res106=$res; 
+echo "London, U.K.			Choopa			$resd"
+serv="speedtest.london.linode.com"; pingcalc; res107=$res; 
+echo "London, U.K.			Linode			$resd"
+serv="lg.lon.psychz.net"; pingcalc; res108=$res; 
+echo "London, U.K.			Psychz			$resd"
+serv="89.46.74.1"; pingcalc; res109=$res; 
+echo "Arrezo, Italy			Aruba Cloud		$resd"
+serv="154.25.2.153"; pingcalc; res111=$res; 
+echo "Rome, Italy			Cogent (Router)		$resd"
+serv="130.117.254.145"; pingcalc; res112=$res; 
+echo "Stockholm, Sweden		Cogent (Router)		$resd"
+serv="130.117.254.137"; pingcalc; res113=$res; 
+echo "Copenhagen, Denmark		Cogent (Router)		$resd"
+serv="81.2.236.1"; pingcalc; res114=$res; 
+echo "Prague, Czech Republic		Aruba Cloud		$resd"
+echo "-----------------------------------------------------------------"
+euavg=$(echo "$res100 $res101 $res102 $res103 $res104 $res105 $res106 $res107 $res108 $res109 $res110 $res111 $res112 $res113 $res114 $scountl" | awk '{print $1/$16 + $2/$16 + $3/$16 + $4/$16 + $5/$16 + $6/$16 + $7/$16 + $8/$16 + $9/$16 + $10/$16 + $11/$16 + $12/$16 + $13/$16 + $14/$16 + $15/$16}');
+echo "Successful servers : $scountl / Average ping : $euavg ms"
+scoreeu=$(echo "$euavg 400" | awk '{print exp(-$1/$2)*100}');
+echo "Score : $scoreeu"
+
+echo " "
+echo " "
+echo "-----------------------------------------------------------------"
+echo "Test Region : Asia & Oceania (China excluded)"
+scountl=0 # Reset region server count
+echo "Location			Provider		Avg ping"
+echo "-----------------------------------------------------------------"
+serv="sgp-ping.vultr.com"; pingcalc; res200=$res; 
+echo "Singapore			Choopa			$resd"
+serv="103.208.85.1"; pingcalc; res201=$res; 
+echo "Singapore			Leaseweb		$resd"
+serv="speedtest.singapore.linode.com"; pingcalc; res202=$res; 
+echo "Singapore			Linode			$resd"
+serv="speedtest.sng01.softlayer.com"; pingcalc; res203=$res; 
+echo "Singapore			Softlayer		$resd"
+serv="speedtest-sgp1.digitalocean.com"; pingcalc; res204=$res; 
+echo "Singapore			DigitalOcean		$resd"
+serv="sgp.infrastructures.ovh"; pingcalc; res205=$res; 
+echo "Singapore			OVH			$resd"
+serv="core1.sel1.he.net"; pingcalc; res220=$res; 
+echo "Seoul, South Korea		HE.net (Router)		$resd"
+serv="hnd-jp-ping.vultr.com"; pingcalc; res206=$res; 
+echo "Tokyo, Japan			Choopa			$resd"
+serv="cnh.romanrm.net"; pingcalc; res207=$res; 
+echo "Tokyo, Japan			GMO Internet		$resd"
+serv="speedtest.tokyo2.linode.com"; pingcalc; res208=$res; 
+echo "Tokyo, Japan			Linode			$resd"
+serv="az.romanrm.net"; pingcalc; res209=$res; 
+echo "Tokyo, Japan			Amazon Web Services	$resd"
+serv="129.250.5.48"; pingcalc; res210=$res; 
+echo "Osaka, Japan			NTT (Router)		$resd"
+serv="cp.tsukaeru.ne.jp"; pingcalc; res211=$res; 
+echo "Nagano, Japan			Tsukaeru.net		$resd"
+serv="103.75.117.1"; pingcalc; res212=$res; 
+echo "Hong Kong			Leaseweb		$resd"
+serv="202.84.157.18"; pingcalc; res222=$res; 
+echo "Hong Kong			Telstra (Router)	$resd"
+serv="lg.tw.psychz.net"; pingcalc; res213=$res; 
+echo "Taipei, Taiwan			Psychz			$resd"
+serv="129.250.2.82"; pingcalc; res214=$res; 
+echo "Taipei, Taiwan			NTT (Router)		$resd"
+serv="speedtest-blr1.digitalocean.com"; pingcalc; res215=$res; 
+echo "Bangalore, India		DigitalOcean		$resd"
+serv="129.250.7.42"; pingcalc; res216=$res; 
+echo "Kuala Lumpur, Malaysia		NTT (Router)		$resd"
+serv="43.255.196.1"; pingcalc; res221=$res; 
+echo "Jakarta, Indonesia		Telkom Indonesia	$resd"
+vserv="syd-au-ping.vultr.com"; pingcalc; res217=$res; 
+echo "Sydney, Australia		Choopa			$resd"
+serv="syd.infrastructures.ovh"; pingcalc; res218=$res; 
+echo "Sydney, Australia		OVH			$resd"
+serv="202.68.64.214"; pingcalc; res219=$res; 
+echo "Sydney, Australia		NTT (Router)		$resd"
+echo "-----------------------------------------------------------------"
+asiavg=$(echo "$res200 $res201 $res202 $res203 $res204 $res205 $res206 $res207 $res208 $res209 $res210 $res211 $res212 $res213 $res214 $res215 $res216 $res217 $res218 $res219 $res220 $res221 $res222 $scountl" | awk '{print $1/$24 + $2/$24 + $3/$24 + $4/$24 + $5/$24 + $6/$24 + $7/$24 + $8/$24 + $9/$24 + $10/$24 + $11/$24 + $12/$24 + $13/$24 + $14/$24 + $15/$24 + $16/$24 + $17/$24 + $18/$24 + $19/$24 + $20/$24 +  $21/$24 + $22/$24 + $23/$24}');
+echo "Successful servers : $scountl / Average ping : $asiavg ms"
+scoreasi=$(echo "$asiavg 400" | awk '{print exp(-$1/$2)*100}');
+echo "Score : $scoreasi"
+
+echo " "
+echo " "
+echo "-----------------------------------------------------------------"
+echo "Test Region : China (Chinese peering)"
+scountl=0 # Reset region server count
+echo "Location			Provider		Avg ping"
+echo "-----------------------------------------------------------------"
+serv="120.26.207.1"; pingcalc; res300=$res; 
+echo "Zhejiang, China			Alibaba Cloud		$resd"
+serv="119.147.101.1"; pingcalc; res301=$res; 
+echo "Guangdong, China		China Telecom		$resd"
+serv="115.159.101.72"; pingcalc; res302=$res; 
+echo "Beijing, China			Tencent Cloud		$resd"
+serv="222.77.179.216"; pingcalc; res303=$res; 
+echo "Fuzhou, China			China Telecom		$resd"
+serv="218.60.94.1"; pingcalc; res304=$res; 
+echo "Liaoning, China			China Unicom		$resd"
+serv="103.25.20.1"; pingcalc; res305=$res; 
+echo "Beijing, China			China Unicom		$resd"
+serv="125.39.240.113"; pingcalc; res306=$res; 
+echo "Tianjin, China			China Unicom		$resd"
+serv="112.29.173.1"; pingcalc; res307=$res; 
+echo "Anhui, China			China Mobile		$resd"
+serv="221.235.205.37"; pingcalc; res308=$res; 
+echo "Hubei, China			China Telecom		$resd"
+serv="182.254.34.87"; pingcalc; res309=$res; 
+echo "Guangdong, China		Tencent Cloud		$resd"
+echo "-----------------------------------------------------------------"
+cnavg=$(echo "$res300 $res301 $res302 $res303 $res304 $res305 $res306 $res307 $res308 $res309 $scountl" | awk '{print $1/$11 + $2/$11 + $3/$11 + $4/$11 + $5/$11 + $6/$11 + $7/$11 + $8/$11 + $9/$11 + $10/$11}');
+echo "Successful servers : $scountl / Average ping : $cnavg ms"
+scorecn=$(echo "$cnavg 500" | awk '{print exp(-$1/$2)*100}');
+echo "Score : $scorecn"
+
+echo " "
+echo " "
+echo "-----------------------------------------------------------------"
+regionalavg=$(echo "$ameavg $euavg $asiavg $cnavg" | awk '{print $1/4 + $2/4 + $3/4 + $4/4}');
+echo "Regional average : $regionalavg ms"
+overalls=$(echo "$scorecdn $scoreame $scoreeu $scoreasi $scorecn" | awk '{print $1/5 + $2/5 + $3/5 + $4/5 + $5/5}');
+echo "Overall peering score : $overalls"
+echo "-----------------------------------------------------------------"
